990c6d8a7ce6a1f0e83406f32aacde29de9ad742
Oleg Dudnik <ecaflol@gmail.com>
2019-11-18 00:38:30 +0300

    third task was uploaded

---
+++ task3/input_form.php
@@ -0,0 +1,17 @@
+<?php
+		echo "  
+			<div>
+				<form action='task3.php' method='post'> 
+					<p>
+						Количество чисел n : <input type='number' size='15' name='quantity' />
+					</p>
+					<p>
+						Число для поиска k : <input type='number' size='15' name='number' />
+					</p>
+					<p>
+						<input type='submit' value='Найти' >
+					</p>
+				</form>
+			</div>";
+
+?>
